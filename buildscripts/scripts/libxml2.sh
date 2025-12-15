1c7206a5829047063e78159393874cf2ca7fc482
WoHal <socianerr@gmail.com>
2025-12-15 12:00:21 +0800

    fix: typo and git clone depth

--- buildscripts/scripts/libxml2.sh
+++ buildscripts/scripts/libxml2.sh
@@ -17,7 +17,7 @@ unset CC CXX # meson wants these unset
 
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
 	-Ddocs=disabled -Ddebugging=disabled -Dhtml=disabled \
-	-Dminimum=true -Dpreifx="$prefix_dir"
+	-Dminimum=true -Dprefix="$prefix_dir"
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
