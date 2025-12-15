eb8ddbb87c6e2cfccbac2cb2052892a3848f4e91
WoHal <socianerr@gmail.com>
2025-12-15 11:21:41 +0800

    enable write_version.sh

--- buildscripts/scripts/ffmpeg.sh
+++ buildscripts/scripts/ffmpeg.sh
@@ -29,7 +29,7 @@ args=(
 	--extra-cflags="-I$prefix_dir/include $cpuflags" --extra-ldflags="-L$prefix_dir/lib"
 
 	--enable-{jni,mediacodec,mbedtls,libdav1d} --disable-vulkan
-	--disable-static --enable-shared --enable-version3
+	--disable-static --enable-shared --disable-gpl --enable-version3
 
 	# disable unneeded parts
 	--disable-{stripping,doc,programs}
