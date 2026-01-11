599365441ea96778d2a7decabe4ed8d06451553f
WoHal <socianerr@gmail.com>
2026-01-11 16:44:36 +0800

    print caller name

--- app/src/main/jni/log.h
+++ app/src/main/jni/log.h
@@ -15,6 +15,7 @@
 __attribute__((noreturn)) void die(const char *msg);
 
 #define CHECK_MPV_INIT() do { \
-	if (__builtin_expect(!g_mpv, 0)) \
+	if (__builtin_expect(!g_mpv, 0))        \
+        __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __func__)    \
         die("libmpv is not initialized"); \
 	} while (0)
